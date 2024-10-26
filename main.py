0d899b3fc0f61b5bd70c820b3464a21c03f7738f
Mohammed-Tahmid-Uddin <mohammedtahmiduddin3@gmail.com>
2024-10-26 19:21:09 +0100

    Displaying Window
    Now the program will actually launch with my title

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from kivy.uix.screenmanager import Screen
-from kivy.uix.floatlayout import FloatLayout
+from kivy.app import App # this helps run the application 
+from kivy.uix.screenmanager import Screen,ScreenManager
+from kivy.uix.floatlayout import FloatLayout # helps with the layout
 
 
-
+''' This class is the main starting page for the program
+ it will include the logo, the login and sign in button'''
 class StartPage(Screen):
     def __init__(self, **kwargs):
         super(StartPage, self).__init__(**kwargs)
@@ -11,6 +13,18 @@ class StartPage(Screen):
         self.add_widget(layout)
      
 
+class MyApp(App):
+    def build(self):
+        # Create a ScreenManager
+        self.title = 'Tahmids budgeting App'
+
+        sm = ScreenManager()
+        
+
+        # Add screens
+        sm.add_widget(StartPage(name='start'))
+        return sm
+
 
 if __name__ == '__main__':
-    StartPage().run()
+    MyApp().run()
