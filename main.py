4651e7a397e0cf63cee2b9f89d9f0e8d649c3ca8
Mohammed-Tahmid-Uddin <mohammedtahmiduddin3@gmail.com>
2024-10-26 19:29:47 +0100

    Creating the Login Button

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from kivy.app import App # this helps run the application
 from kivy.uix.screenmanager import Screen,ScreenManager
 from kivy.uix.floatlayout import FloatLayout # helps with the layout
 from kivy.uix.button import Button
+from kivy.uix.image import Image
+
 
 
 ''' This class is the main starting page for the program
@@ -9,19 +11,24 @@ from kivy.uix.button import Button
 class StartPage(Screen):
     def __init__(self, **kwargs):
         super(StartPage, self).__init__(**kwargs)
-        layout = FloatLayout()  # Switch to FloatLayout for custom positioning
+        layout = FloatLayout()  # Switchs to FloatLayout for custom positioning
         self.add_widget(layout)# This diplays the page
+
 
         # Buttons for sign up and login
         signup_button = Button(text='Sign up page', size_hint=(None, None), size=(200, 50),
-                               pos_hint={'center_x': 0.5, 'y': 0.9})  # Centered horizontally, 50% up from bottom
+                               pos_hint={'center_x': 0.5, 'y': 0.9})  # Centeres horizontally, 50% up from bottom
         layout.add_widget(signup_button)# this displays the button
      
 
+        login_button = Button(text='Login page', size_hint=(None, None), size=(200, 50),
+                              pos_hint={'center_x': 0.5, 'y': 0.8})  # Centered horizontally, 30% up from bottom
+        layout.add_widget(login_button)
+
 class MyApp(App):
     def build(self):
-        # Create a ScreenManager
-        self.title = 'Tahmids budgeting App'
+        # Creates a ScreenManager
+        self.title = 'Tahmids budgeting App' # This is the title of the window
 
         sm = ScreenManager()
         
