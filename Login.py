1e974615ff84a086a5f51e8e6513a77e809949db
Mohammed-Tahmid-Uddin <mohammedtahmiduddin3@gmail.com>
2024-10-30 19:35:13 +0000

    Finshing the Login Window

--- Login.py
+++ Login.py
@@ -15,6 +15,24 @@ class LoginPage(Screen):
 
         self.add_widget(layout)
 
+        # Adds username label
+        layout.add_widget(Label(text='Username', size_hint=(None, None), size=(200, 30), pos_hint={'center_x': 0.5, 'top': 0.9}))
+
+        # Adds text input for username
+        self.student_username_box = TextInput(multiline=False, size_hint=(None, None), size=(200, 50), pos_hint={'center_x': 0.5, 'top': 0.85})
+        layout.add_widget(self.student_username_box)
+
+        # Adds password label
+        layout.add_widget(Label(text='Password', size_hint=(None, None), size=(200, 30), pos_hint={'center_x': 0.5, 'top': 0.775}))
+
+        # Adds text input for password
+        self.student_password_box = TextInput(multiline=False, password=True, size_hint=(None, None), size=(200, 50), pos_hint={'center_x': 0.5, 'top': 0.725})
+        layout.add_widget(self.student_password_box)
+
+        # Adds login button
+        login_button = Button(text="Login", size_hint=(None, None), size=(200, 50), pos_hint={'center_x': 0.5, 'top': 0.675})
+        layout.add_widget(login_button)
+
 
 
 class MyApp(App):
