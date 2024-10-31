88ecd17ed3c2ff79d2faaa7861b9624a96be9eba
Mohammed-Tahmid-Uddin <mohammedtahmiduddin3@gmail.com>
2024-10-31 18:26:20 +0000

    DropDown list created

--- EditFinances.py
+++ EditFinances.py
@@ -2,6 +2,9 @@ from kivy.app import App
 from kivy.uix.screenmanager import Screen
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.button import Button
+from kivy.uix.dropdown import DropDown
+import sqlite3
+
 
 ''' THIS C;ASS IS CREATED IN ORDER FOR THE USERS TO ENTER THEIR TRANSCATIONS'''
 class EditFinances(Screen):
@@ -14,3 +17,18 @@ class EditFinances(Screen):
         # Create a database connection
         self.conn = sqlite3.connect('Students.db')
         self.cursor = self.conn.cursor()
+
+        # Creating the dropdown menu
+        self.month_button = DropDown()
+        self.item_button = DropDown()
+
+        # Lists of months
+        months = [
+            'January', 'February', 'March', 'April', 'May', 'June',
+            'July', 'August', 'September', 'October', 'November', 'December'
+        ]
+
+        # Lists of items
+        items = [
+            'Food', 'Household expenses', 'Travel', 'Shopping', 'Self Care', 'Other'
+        ]
